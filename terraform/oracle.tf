1c0a9c971bcfddebf0d8cb7f476f314d8993cc96
Michael Hollister <hllism@amazon.com>
2020-11-16 23:54:01 -0600

    locking down functioning

---
+++ terraform/oracle.tf
@@ -0,0 +1,24 @@
+provider "oci" {
+  tenancy_ocid = var.tenancy
+  user_ocid = var.user
+  fingerprint = var.fingerprint
+  private_key_path = var.keypath
+  region = "us-phoenix-1"
+}
+
+resource "oci_core_instance" "test_instance" {
+  availability_domain = "yuqr:PHX-AD-1"
+  shape = "VM.Standard.E2.1.Micro"
+  compartment_id = var.tenancy
+  create_vnic_details {
+      subnet_id = var.subnetid
+  }
+  metadata = {
+      ssh_authorized_keys = file(var.sshkeyfile)
+  }
+  source_details {
+      source_type = "image"
+      source_id = "ocid1.image.oc1.phx.aaaaaaaaky4luenz7yvuzz26zipiun6dzbkm7hkon7tppynpm2l6p32aen7a"
+  }
+
+}
---
+++ terraform/variables.tf
@@ -0,0 +1,18 @@
+variable "tenancy" {
+	type = string
+}
+variable "user" {
+	type = string
+}
+variable "fingerprint" {
+	type = string
+}
+variable "keypath" {
+	type = string
+}
+variable "subnetid" {
+	type = string
+}
+variable "sshkeyfile" {
+  type = string
+}
